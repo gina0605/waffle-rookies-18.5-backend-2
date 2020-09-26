e94b598f75fb255b85abce3ca4e223a6f9550c1b
gina0605 <69783927+gina0605@users.noreply.github.com>
2020-09-26 21:58:01 +0900

    task3 Updated

--- waffle_backend/user/models.py
+++ waffle_backend/user/models.py
@@ -2,16 +2,18 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
-class ParticipantProfile(models.model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
+class ParticipantProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE,
+                                related_name='participant_profile', primary_key=True)
     university = models.CharField(max_length=50, blank=True)
     year = models.PositiveSmallIntegerField(null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
 
-class InstructorProfile(models.model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
+class InstructorProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE,
+                                related_name='instructor_profile', primary_key=True)
     company = models.CharField(max_length=50, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
