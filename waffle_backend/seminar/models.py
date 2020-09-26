c06e878bef6d2b069adafbff136d454b15cf01cd
gina0605 <69783927+gina0605@users.noreply.github.com>
2020-09-26 19:46:54 +0900

    task2 Done

--- waffle_backend/seminar/models.py
+++ waffle_backend/seminar/models.py
@@ -1,8 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class Seminar(models.Model):
     name = models.CharField(unique=True, max_length=100)
     description = models.CharField(max_length=200, blank=True)
     start_date = models.DateField(null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
+
+class UserSeminar(models.Model):
+    user = models.ForeignKey(User, related_name='user_seminar', on_delete=models.CASCADE)
+    seminar = models.ForeignKey(Seminar, related_name='user_seminar', on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
