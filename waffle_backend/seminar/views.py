02228cd16687592c3afd479cbf3b263f38151f98
gina0605 <69783927+gina0605@users.noreply.github.com>
2020-09-29 02:41:24 +0900

    Added GET seminar
    retrieve and list

--- waffle_backend/seminar/views.py
+++ waffle_backend/seminar/views.py
@@ -1,6 +1,6 @@
 from django.core.exceptions import ObjectDoesNotExist
 from rest_framework import status, viewsets
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
 from seminar.models import Seminar, UserSeminar
 from seminar.serializers import SeminarSerializer
@@ -10,6 +10,12 @@ class SeminarViewSet(viewsets.GenericViewSet):
     queryset = Seminar.objects.all()
     serializer_class = SeminarSerializer
     permission_classes = (IsAuthenticated, )
+
+    def get_permissions(self):
+        if self.action in ('retrieve', 'list'):
+            return (AllowAny(), )
+        else:
+            return super(SeminarViewSet, self).get_permissions()
 
     def create(self, request):
         user = request.user
@@ -44,3 +50,11 @@ class SeminarViewSet(viewsets.GenericViewSet):
         serializer.is_valid(raise_exception=True)
         serializer.update(seminar, serializer.validated_data)
         return Response(serializer.data)
+
+    def retrieve(self, request, pk=None):
+        seminar = self.get_object()
+        return Response(self.get_serializer(seminar).data)
+
+    def list(self, request):
+        seminars = self.get_queryset().prefetch_related('user_seminar')
+        return Response(self.get_serializer(seminars, many=True).data)
