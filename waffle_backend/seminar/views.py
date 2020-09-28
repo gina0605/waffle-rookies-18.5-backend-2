ec2bc0f8c029a636c8738d74dfb29a2785e30524
gina0605 <69783927+gina0605@users.noreply.github.com>
2020-09-29 04:07:24 +0900

    Added POST /seminar/pk/user/

--- waffle_backend/seminar/views.py
+++ waffle_backend/seminar/views.py
@@ -2,6 +2,7 @@ from django.core.exceptions import ObjectDoesNotExist
 from rest_framework import status, viewsets
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
+from rest_framework.decorators import action
 from seminar.models import Seminar, UserSeminar
 from seminar.serializers import SeminarSerializer, SimpleSeminarSerializer
 
@@ -25,7 +26,10 @@ class SeminarViewSet(viewsets.GenericViewSet):
     def create(self, request):
         user = request.user
         if not hasattr(user, 'instructor'):
-            return Response({"error": "Only instructors can create seminars"}, status=status.HTTP_403_FORBIDDEN)
+            return Response(
+                {"error": "Only instructors can create seminars"},
+                status=status.HTTP_403_FORBIDDEN
+            )
         print(user.instructor)
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
@@ -44,13 +48,15 @@ class SeminarViewSet(viewsets.GenericViewSet):
         if not UserSeminar.objects.filter(user=user, seminar=seminar, role='instructor').exists():
             return Response(
                 {"error": "Only instructors of this seminar can change information"},
-                status=status.HTTP_403_FORBIDDEN)
+                status=status.HTTP_403_FORBIDDEN
+            )
 
         participants = UserSeminar.objects.filter(seminar=seminar, role='participant').count()
         if 'capacity' in request.data and request.data.get('capacity') < participants:
             return Response(
                 {"error": "Cannot set capacity less than the number of participants"},
-                status=status.HTTP_400_BAD_REQUEST)
+                status=status.HTTP_400_BAD_REQUEST
+            )
         serializer = self.get_serializer(seminar, data=request.data, partial=True)
         serializer.is_valid(raise_exception=True)
         serializer.update(seminar, serializer.validated_data)
@@ -67,3 +73,48 @@ class SeminarViewSet(viewsets.GenericViewSet):
         if param.get('order', '') == 'earliest':
             seminars.order_by('created_at')
         return Response(self.get_serializer(seminars, many=True).data)
+
+    @action(detail=True, methods=['POST'])
+    def user(self, request, pk):
+        user = request.user
+        seminar = self.get_object()
+        role = request.data.get('role', '')
+        if role not in ('participant', 'instructor'):
+            return Response(
+                {"error": "Role should be participant or instructor"},
+                status=status.HTTP_400_BAD_REQUEST)
+        if not hasattr(user, role):
+            return Response(
+                {"error": "The user is not a {}".format(role)},
+                status=status.HTTP_403_FORBIDDEN
+            )
+        if UserSeminar.objects.filter(user=user, seminar=seminar).exists():
+            return Response(
+                {"error": "The user is already a member of the seminar"},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+        if role == 'participant':
+            if not user.participant.accepted:
+                return Response(
+                    {"error": "The user is not accepted"},
+                    status=status.HTTP_403_FORBIDDEN
+                )
+            participants = UserSeminar.objects.filter(seminar=seminar, role='participant').count()
+            if participants == seminar.capacity:
+                return Response(
+                    {"error": "This seminar is already full"},
+                    status=status.HTTP_400_BAD_REQUEST
+                )
+        elif role == 'instructor':
+            if UserSeminar.objects.filter(user=user, role='instructor').exists():
+                return Response(
+                    {"error": "The user is an instructor of another seminar"},
+                    status=status.HTTP_400_BAD_REQUEST
+                )
+
+        UserSeminar.objects.create(
+            user=user,
+            seminar=seminar,
+            role=role,
+        )
+        return Response(self.get_serializer(seminar).data, status=status.HTTP_201_CREATED)
