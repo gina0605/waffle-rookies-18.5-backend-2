ba306b9162c3c81d7e50a2a389c5954c7c97dbcb
gina0605 <69783927+gina0605@users.noreply.github.com>
2020-09-30 02:31:47 +0900

    Updated Seminar model
    name is not unique

--- waffle_backend/seminar/models.py
+++ waffle_backend/seminar/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 
 
 class Seminar(models.Model):
-    name = models.CharField(unique=True, max_length=50)
+    name = models.CharField(max_length=50)
     description = models.CharField(max_length=200, blank=True)
     capacity = models.PositiveSmallIntegerField()
     count = models.PositiveSmallIntegerField()
