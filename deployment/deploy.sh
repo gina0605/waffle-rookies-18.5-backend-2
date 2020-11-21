3e4a17cad157f10d208901a9f8b39c39f442e465
EC2 Default User <ec2-user@ip-172-31-3-138.ap-northeast-2.compute.internal>
2020-11-21 17:15:34 +0000

    Update deploy.sh, nginx conf

--- deployment/deploy.sh
+++ deployment/deploy.sh
@@ -4,6 +4,8 @@ source ~/.bash_profile
 
 cd ~/waffle-rookies-18.5-backend-2
 git pull origin deploy
+sudo cp deployment/nginx.conf /etc/nginx/nginx.conf
+sudo cp deployment/sites-available/waffle-backend.conf /etc/nginx/sites-available/waffle-backend.conf
 
 pyenv activate waffle-backend
 pip install -r requirements.txt
